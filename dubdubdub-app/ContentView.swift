7e7dd72c706725131ec5b371baaa7c89fa82de75
lysonjeada <lyscalefi@gmail.com>
2024-07-29 21:39:11 -0300

    add initial structure with images

---
+++ dubdubdub-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dubdubdub-app
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dubdubdub-app/dubdubdub_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dubdubdub_appApp.swift
+//  dubdubdub-app
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct dubdubdub_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
